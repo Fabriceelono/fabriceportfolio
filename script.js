75bba04050d30144c8e6a802dcc025182d4a574c
Fabrice ELONO PISETH <r0912443@student.thomasmore.be>
2025-03-24 23:56:02 +0100

    Update assets and enhance CV with new content; add VSCode settings and typing animation in script.js

--- script.js
+++ script.js
@@ -13,3 +13,48 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("scrolled");
   }
 });
+
+const introText = "Hello, I'm ";
+const nameText = "Fabrice Elono";
+
+const typedText = document.getElementById("typed-text");
+const typedName = document.getElementById("typed-name");
+const cursor1 = document.getElementById("cursor-1");
+const cursor2 = document.getElementById("cursor-2");
+
+let index = 0;
+let nameIndex = 0;
+
+function typeIntro() {
+  if (index < introText.length) {
+    typedText.textContent += introText.charAt(index);
+    index++;
+    setTimeout(typeIntro, 100);
+  } else {
+    cursor1.style.display = "none"; // hide first cursor
+    cursor2.style.display = "inline-block"; // show second
+    typeName(); // start next typing
+  }
+}
+
+function typeName() {
+  if (nameIndex < nameText.length) {
+    typedName.textContent += nameText.charAt(nameIndex);
+    nameIndex++;
+    setTimeout(typeName, 120);
+  } else {
+    cursor2.style.display = "none";
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  cursor2.style.display = "none"; // hide second cursor initially
+  typeIntro();
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const profileImage = document.querySelector(".section__pic-container");
+  if (profileImage) {
+    profileImage.classList.add("floating-image");
+  }
+});
